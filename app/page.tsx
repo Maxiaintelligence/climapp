3bd19de74aab8cdedfe1fa6fde39f57ed6c583a1
Maxiaintelligence <inteligenciamaxia@gmail.com>
2025-11-05 19:44:49 -0600

    Initial project structure and data file

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Bienvenido a Climapp</h1>
+      <p>Monitor de Riesgos Climáticos de la Pastoral Social Arquidiócesis Tulancingo.</p>
+    </div>
+  );
+}
